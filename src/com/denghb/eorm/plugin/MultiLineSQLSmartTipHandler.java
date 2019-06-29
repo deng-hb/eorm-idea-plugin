8809fae672ce1582f4fdce4e7670ab53fb28cc8d
denghb <i@denghb.com>
2019-06-29 18:15:17 +0800

    SQL字段智能提示

--- src/com/denghb/eorm/plugin/MultiLineSQLSmartTipHandler.java
+++ src/com/denghb/eorm/plugin/MultiLineSQLSmartTipHandler.java
@@ -11,51 +11,98 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiFile;
-import net.sf.jsqlparser.parser.CCJSqlParserUtil;
-import net.sf.jsqlparser.schema.Table;
-import net.sf.jsqlparser.statement.select.Join;
-import net.sf.jsqlparser.statement.select.PlainSelect;
-import net.sf.jsqlparser.statement.select.Select;
-import net.sf.jsqlparser.statement.select.SelectBody;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.*;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Properties;
 
 /**
  * @author denghb 2019-06-27 00:16
  */
 public class MultiLineSQLSmartTipHandler extends TypedHandlerDelegate {
 
+    private static final String EORM_CONFIG = "eorm.config";
     private static final String DB_URL = "jdbc:mysql://%s:%s/%s?useUnicode=true&amp;characterEncoding=utf-8";
-    private Connection connection;
+    private static final String sql1 = "SELECT column_name,column_type,column_comment FROM information_schema.COLUMNS WHERE table_schema = ? AND table_name = ? ";
 
     private Eorm db;
-
     private String schema;
-
-    private String sql1 = "SELECT column_name FROM information_schema.COLUMNS WHERE table_schema = ? AND table_name = ? ";
-
-    private Map<String, String> aliasTableHub = new HashMap<String, String>();
+    private Connection connection;
 
     public MultiLineSQLSmartTipHandler() {
         System.out.println("MultiLineSQLSmartTipHandler init");
     }
 
+    @NotNull
+    @Override
+    public Result beforeCharTyped(char c, @NotNull Project project, @NotNull Editor editor, @NotNull PsiFile file, @NotNull FileType fileType) {
+        if (EORM_CONFIG.equals(file.getName())) {
+            close();
+        }
+        if ('.' != c) {
+            return super.beforeCharTyped(c, project, editor, file, fileType);
+        }
+
+        Document document = editor.getDocument();
+        int caretOffset = editor.getCaretModel().getOffset();
+
+        String text = document.getText();
+        String sql = getSql(caretOffset, text);
+        if (null == sql) {
+            return super.beforeCharTyped(c, project, editor, file, fileType);
+        }
+
+        document.insertString(caretOffset, String.valueOf(c));
+        editor.getCaretModel().moveToOffset(caretOffset + 1);
+
+        // 获取表名
+        String tableName = getTableName(caretOffset, text, sql);
+        if (null != tableName) {
+
+            conn(project.getBasePath());
+
+            if (null == db) {
+                return Result.STOP;
+            }
+
+            List<Column> columns = db.select(Column.class, sql1, schema, tableName);
+            if (null == columns || columns.isEmpty()) {
+                return Result.STOP;
+            }
+            LookupElement[] lookupElement = new LookupElement[columns.size()];
+            for (int i = 0; i < lookupElement.length; i++) {
+                Column column = columns.get(i);
+                lookupElement[i] = LookupElementBuilder.create(column.getColumnName())
+                        .withTailText(" " + column.getColumnComment(), true)
+                        .withTypeText(column.getColumnType());
+            }
+            LookupManager.getInstance(project).showLookup(editor, lookupElement);
+        }
+
+        return Result.STOP;
+    }
+
+
     private void conn(String basePath) {
+
         try {
-            if (null != connection) {
+            if (null != db) {
                 return;
             }
-            Properties properties = new Properties();
-            File file1 = new File(basePath + "/eorm.config");
+            File file1 = new File(basePath + "/" + EORM_CONFIG);
             if (!file1.exists()) {
                 return;
             }
-            properties.load(new FileInputStream(file1));
+            Properties properties = new Properties();
+            FileInputStream is = new FileInputStream(file1);
+            properties.load(is);
+            is.close();
+
             String username = properties.getProperty("username");
             String password = properties.getProperty("password");
             String port = properties.getProperty("port");
@@ -70,41 +117,31 @@ public class MultiLineSQLSmartTipHandler extends TypedHandlerDelegate {
 
             db = new EormImpl(connection);
 
-            System.out.println("Connection ...");
+            System.out.println("Connected");
         } catch (Exception e) {
             e.printStackTrace();
+            close();
         }
     }
 
-    public void testParseAlias(String sql) {
-        try {
-            Select select = (Select) CCJSqlParserUtil.parse(sql);
-            SelectBody selectBody = select.getSelectBody();
-            PlainSelect plainSelect = (PlainSelect) selectBody;
-            Table table = (Table) plainSelect.getFromItem();
+    private void close() {
+        if (null != db) {
+            db = null;
+        }
+        if (null != connection) {
+            try {
+                connection.close();
+                connection = null;
+            } catch (SQLException ex) {
 
-            aliasTableHub.put(table.getName(), table.getName());
-
-            if (table.getAlias() != null) {
-                aliasTableHub.put(table.getAlias().getName(), table.getName());
             }
-            if (null != plainSelect.getJoins()) {
-
-                for (Join join : plainSelect.getJoins()) {
-                    Table table1 = (Table) join.getRightItem();
-                    if (table1.getAlias() != null) {
-                        aliasTableHub.put(table1.getAlias().getName(), table1.getName());
-
-                        aliasTableHub.put(table.getName(), table.getName());
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
     private String getSql(int caretOffset, String text) {
+        if (!text.contains("\"\"/*{") || !text.contains("}*/;")) {
+            return null;
+        }
         StringBuilder sb = new StringBuilder();
         // 往前找 /*{
         for (int i = caretOffset; i > 0; i--) {
@@ -130,51 +167,102 @@ public class MultiLineSQLSmartTipHandler extends TypedHandlerDelegate {
         return sb.toString();
     }
 
-    @NotNull
-    @Override
-    public Result beforeCharTyped(char c, @NotNull Project project, @NotNull Editor editor, @NotNull PsiFile file, @NotNull FileType fileType) {
-        conn(project.getBasePath());
+    private static String getTableName(int caretOffset, String text, String sql) {
 
-        Document document = editor.getDocument();
-        String text = document.getText();
-        int caretOffset = editor.getCaretModel().getOffset();
-        String sql = getSql(caretOffset, text);
-        if (' ' == c && null != sql) {
-            testParseAlias(sql);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = caretOffset - 1; i > 0; i--) {
+            char c = text.charAt(i);
+            if (' ' == c || ',' == c || '\n' == c) {
+                break;
+            }
+            sb.append(c);
         }
-        if ('.' == c && null != sql) {
-            document.insertString(caretOffset, String.valueOf(c));
-            editor.getCaretModel().moveToOffset(caretOffset + 1);
+        if (sb.length() == 0) {
+            return null;
+        }
+        String alias = sb.reverse().toString().toLowerCase();
 
-            // 获取表名
-            if (' ' != text.charAt(caretOffset - 1)) {
-                StringBuilder tableName = new StringBuilder();
-                for (int i = caretOffset - 1; i > 0; i--) {
-                    char c2 = text.charAt(i);
-                    if (' ' == c2 || ',' == c2) {
-                        break;
-                    }
-                    tableName.append(c2);
-                }
-
-                tableName.reverse();
-                String table = tableName.toString();
-
-                table = aliasTableHub.get(table);
-                if (null != table) {
-
-                    List<String> tableNames = db.select(String.class, sql1, schema, table);
-                    LookupElement[] lookupElement = new LookupElement[tableNames.size()];
-                    for (int i = 0; i < lookupElement.length; i++) {
-                        lookupElement[i] = LookupElementBuilder.create(tableNames.get(i));
-                    }
-                    LookupManager.getInstance(project).showLookup(editor, lookupElement);
-                }
+        // 转小写来匹配获取 from xxx ...  alias 结束这段
+        String sql1 = sql.toLowerCase();
+        // 排除换行等符号影响
+        sql1 = sql1.replaceAll("\n", " ")
+                .replaceAll("\r", " ")
+                .replaceAll("\t", " ");
+        String ss = sql1.substring(sql1.indexOf(" from "));
+        if (ss.length() > 6) {
+            ss = ss.replaceAll(",", " ,");
+            int end = ss.indexOf(" " + alias + " ");
+            if (end < 6) {
+                return alias;
             }
 
+            ss = ss.substring(6, end).trim();
 
-            return Result.STOP;
+            StringBuilder tableName = new StringBuilder();
+            for (int j = ss.length() - 1; j >= 0; j--) {
+                char c = ss.charAt(j);
+                if (' ' == c || ',' == c || '\n' == c) {
+                    break;
+                }
+                tableName.append(c);
+
+            }
+            if (tableName.length() == 0) {
+                return alias;
+            }
+            String name = tableName.reverse().toString();
+            if ("join".equals(name)) {
+                return alias;
+            }
+            if (name.endsWith(")")) {
+                return null;
+            }
+            return name;
         }
-        return super.beforeCharTyped(c, project, editor, file, fileType);
+
+        return alias;
+    }
+
+    public static class Column {
+
+        public String columnName;
+
+        public String columnType;
+
+        public String columnComment;
+
+        public String getColumnName() {
+            return columnName;
+        }
+
+        public void setColumnName(String columnName) {
+            this.columnName = columnName;
+        }
+
+        public String getColumnType() {
+            return columnType;
+        }
+
+        public void setColumnType(String columnType) {
+            this.columnType = columnType;
+        }
+
+        public String getColumnComment() {
+            return columnComment;
+        }
+
+        public void setColumnComment(String columnComment) {
+            this.columnComment = columnComment;
+        }
+
+        @Override
+        public String toString() {
+            return "Column{" +
+                    "columnName='" + columnName + '\'' +
+                    ", columnType='" + columnType + '\'' +
+                    ", columnComment='" + columnComment + '\'' +
+                    '}';
+        }
     }
 }
